970f838349d95903cb482378aa2f5a7008f57f01
Arek Olek <arekolek+git@gmail.com>
2012-11-18 10:42:56 +0100

    add roboguice dependency

--- client/knurek/src/main/java/com/arekolek/knurek/HelloAndroidActivity.java
+++ client/knurek/src/main/java/com/arekolek/knurek/HelloAndroidActivity.java
@@ -4,22 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class HelloAndroidActivity extends Activity {
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
 
-    private static String TAG = "knurek";
-
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-		Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
-    }
+@ContentView(R.layout.main)
+public class HelloAndroidActivity extends RoboActivity {
 
 }
 
