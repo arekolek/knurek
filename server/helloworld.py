545d2c1f5171778a1ccc8aae8b32e671d0d792ef
Arek Olek <arekolek+git@gmail.com>
2012-11-19 03:53:23 +0100

    update main page to return friends data

--- server/helloworld.py
+++ server/helloworld.py
@@ -1,5 +1,7 @@
 import webapp2
 
+import json
+
 import pylast
 
 from lastapikeys import API_KEY, API_SECRET
@@ -7,8 +9,8 @@ from lastapikeys import API_KEY, API_SECRET
 class MainPage(webapp2.RequestHandler):
   def get(self):
     network = pylast.LastFMNetwork(api_key=API_KEY,api_secret=API_SECRET)  
-    self.response.headers['Content-Type'] = 'text/html'
-    self.response.write('Hello, webapp2 World! <img src="'+network.get_artist("Air").get_cover_image()+'" />')
+    self.response.headers['Content-Type'] = 'application/json'
+    self.response.write(json.dumps([{'name':f.get_name(),'realname':f.get_real_name(),'image':f.get_image()} for f in network.get_user(self.request.GET['user']).get_friends()[:10]]))
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
