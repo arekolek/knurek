26c96088d1baae1dc08ff36b820f52c524f9e73d
Arek Olek <arekolek+git@gmail.com>
2012-12-10 07:01:54 +0100

    save session

--- server/src/auth.py
+++ server/src/auth.py
@@ -3,8 +3,8 @@ import webapp2, json
 from google.appengine.ext import db
 import base64
 import os
-from lastapikeys import API_KEY
-import urllib
+from lastapikeys import API_KEY, API_SECRET
+from lib import pylast
 
 class User(db.Model):
     name = db.StringProperty()
@@ -22,18 +22,27 @@ class AuthPage(webapp2.RequestHandler):
                 pass
             else:
                 if user.session == None:
+                    network = pylast.LastFMNetwork(api_key=API_KEY,api_secret=API_SECRET)
+                    keyGen = pylast.SessionKeyGenerator(network)
                     if 'token' not in self.request.GET:
                         # redirect to api auth with callback here
-                        callback = 'http://' + self.request.headers['Host'] + '/api/auth?identifier=' + identifier 
-                        url = 'http://www.last.fm/api/auth?' + urllib.urlencode({'api_key': API_KEY, 'cb': callback})
-                        self.redirect(url)
+                        callback = 'http://' + self.request.headers['Host'] + '/api/auth/?identifier=' + identifier 
+                        self.redirect(keyGen.get_web_auth_url(callback))
                     else:
                         # get session
                         # show a webpage telling to close the browser
-                        pass
+                        token = self.request.GET['token']
+                        result = keyGen.get_web_auth_session_key(token)
+                        user.name = result['name']
+                        user.session = result['key']
+                        user.put()
+                        
+                        self.response.headers['Content-Type'] = 'text/html'
+                        self.response.write('<html><body>Well done ' + user.name + '. You can now go back to the app.</body></html>')
                 else:
                     # return username
-                    pass
+                    self.response.headers['Content-Type'] = 'application/json'
+                    self.response.write(json.dumps({"name": user.name}))
         else:
             # create new user
             identifier =  base64.urlsafe_b64encode(os.urandom(30))
