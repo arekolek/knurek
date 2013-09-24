208ec71b17373f0fc19dbfb66c8540225e548b5c
Arek Olek <arekolek+git@gmail.com>
2013-09-24 12:27:33 -0400

    update api url

--- client/knurek/src/main/java/com/github/arekolek/knurek/auth/Constants.java
+++ client/knurek/src/main/java/com/github/arekolek/knurek/auth/Constants.java
@@ -2,11 +2,10 @@ package com.github.arekolek.knurek.auth;
 
 public class Constants {
 
-
     public static final String ACCOUNT_TYPE = "com.github.arekolek.knurek";
     public static final String AUTH_TOKEN_TYPE = "com.github.arekolek.knurek";
-    private static final String STAGING = "http://192.168.1.102:54321";
+    private static final String STAGING = "http://192.168.1.103:54321";
     private static final String PRODUCTION = "http://knurekapi.appspot.com";
-    public static final String ROOT_URL = STAGING;
+    public static final String ROOT_URL = PRODUCTION;
 
 }
